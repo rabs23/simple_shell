d98890eabd98b8debf9e87d1a37c1ac8621ff78f
root <ruthrabs22@gmail.com>
2023-11-14 11:25:10 -0800

    1-prompt.c

---
+++ 1-prompt.c
@@ -0,0 +1,33 @@
+#include <shell.h>
+
+/**
+ * prompt - program that displays a prompt and waits for a user to type command
+ */
+
+void prompt(void)
+{
+	PRINTER("$ ");
+}
+/**
+ * prints_error - function that displays errorbased on command and how
+ * many times the shell loops
+ * @value: user input
+ * @count: loop count
+ * @argv: name of the program
+ * Return: void
+ */
+
+void prints_error(char *value, int count, char **argv)
+{
+	char *e;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	e = _itoa(count);
+	PRINTER(e);
+	free(e);
+	PRINTER(": ");
+	PRINTER(value);
+	PRINTER(": not found\n");
+}
+
